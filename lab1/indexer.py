c04f68a34ba32b5ae327184817e0a9932da8b2a4
Ovhagen <ovhagen@gmail.com>
2017-08-31 18:59:04 +0200

    Changed regex pattern, added tests, does not give correct result yet

--- lab1/indexer.py
+++ lab1/indexer.py
@@ -6,8 +6,7 @@ from collections import defaultdict
 
 
 def words(text):
-    return re.finditer(r'[\wåäö]+', text.lower())
-
+    return re.finditer(r'\p{L}+', text.lower())
 
 def fileindex(filename):
     with codecs.open("Selma/"+filename+".txt", 'r', encoding='utf8') as f:
@@ -25,4 +24,13 @@ def fileindex(filename):
 
     pickle.dump(dic, open(filename+".idx", "wb"))
 
-print(pickle.load(open('bannlyst'+".idx", "rb")))
+
+fileindex('test') #Starts generating dictionary from bannlyst.txt
+
+#Tests
+dic = pickle.load(open('bannlyst'+".idx", "rb"))
+print(dic)
+
+print("gjord", dic.get('gjord'))            #Should be [8551, 183692, 220875]
+print("uppklarnade",dic.get('uppklarnade')) #Should be [8567]
+print("stjärnor",dic.get('stjärnor'))       #Should be [8590]

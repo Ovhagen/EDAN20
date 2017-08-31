efae51b9aef86e02bb24982daa42b6fd900c7798
Ovhagen <te11c.pontus.ovhagen@gmail.com>
2017-08-31 18:27:13 +0200

    Update indexer.py

--- lab1/indexer.py
+++ lab1/indexer.py
@@ -12,7 +12,6 @@ def words(text):
 def fileindex(filename):
     with codecs.open("Selma/"+filename+".txt", 'r', encoding='utf8') as f:
         text = f.read()
-
     ws = words(text)
     dic = defaultdict()
     for match in ws:
@@ -22,7 +21,10 @@ def fileindex(filename):
             dic[word].append(ind)
         else:
             dic[word] = [ind]
-
+            
     pickle.dump(dic, open(filename+".idx", "wb"))
 
+
+fileindex('bannlyst') #Starts generating dictionary
+    
 print(pickle.load(open('bannlyst'+".idx", "rb")))

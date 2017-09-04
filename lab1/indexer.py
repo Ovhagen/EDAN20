dd469847f5b9233544b4adacd3019197537822c2
Ovhagen <ovhagen@gmail.com>
2017-09-04 11:52:22 +0200

    Added master index and enabled input through console

--- lab1/indexer.py
+++ lab1/indexer.py
@@ -3,32 +3,66 @@ import pickle
 import codecs
 from collections import Counter
 from collections import defaultdict
+import os
+import sys
 
 
 def words(text):
     return re.finditer(r'\p{L}+', text.lower())
 
-def fileindex(filename):
-    with codecs.open("Selma/"+filename+".txt", 'r', encoding='utf8') as f:
-        text = f.read()
-    ws = words(text)
-    dic = defaultdict()
+def get_files(dir, suffix):
+    """
+    Returns all the files in a folder ending with suffix
+    :param dir:
+    :param suffix:
+    :return: the list of file names
+    """
+    files = []
+    for file in os.listdir(dir):
+        if file.endswith(suffix):
+            files.append(file)
+    return files
+
+def words_to_index(ws):
+    word_idx = {}
     for match in ws:
         word = match.group()
         ind = match.start()
-        if (dic.get(word)):
-            dic[word].append(ind)
+        if (word_idx.get(word)):
+            word_idx[word].append(ind)
         else:
-            dic[word] = [ind]
+            word_idx[word] = [ind]
+    return word_idx
 
-    pickle.dump(dic, open(filename+".idx", "wb"))
+def fileindex(dir):
+    master_index = {}
+    files = get_files(dir, ".txt")
+    for file in files:
+        with codecs.open(dir+file, 'rb', encoding='utf-8') as f:
+            text = f.read()
+        ws = words(text)
+        idx = words_to_index(ws)
+        for word in idx:
+            if word in master_index:
+                master_index[word][file] = idx[word]
+            else:
+                master_index[word] = {}
+                master_index[word][file] = idx[word]
+
+    pickle.dump(master_index, open("master_index" + ".idx", "wb")) #[:-4] to remove .txt part of string
 
 
-fileindex('bannlyst') #Starts generating dictionary from bannlyst.txt
+#Main
+arguments = sys.argv
+if arguments[1] != '':
+    print("Search in folder", arguments[1])
+    fileindex(arguments[1]+'/') #Starts generating indexed dictionary from files
+else:
+    raise ValueError("Invalid argument. Input should be the directory from where files should be read.")
+
 
 #Tests
-dic = pickle.load(open('bannlyst'+".idx", "rb"))
-print(dic)
-print("gjord", dic.get('gjord'))            #Should be [8551, 183692, 220875]
-print("uppklarnade",dic.get('uppklarnade')) #Should be [8567]
-print("stjärnor",dic.get('stjärnor'))       #Should be [8590]
+dic = pickle.load(open('master_index'+".idx", "rb"))
+print('gjord:', dic['gjord']) #Should be [8551, 183692, 220875]
+#print(dic['uppklarnade']) #Should be [8567]
+print('stjärnor:', dic['stjärnor']) #Should be [8590]
